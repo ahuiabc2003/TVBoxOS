7f80d0fd9729785aa644a24fa45f01d18e40fc0e
ahuiabc2003 <54199906+ahuiabc2003@users.noreply.github.com>
2023-08-02 17:41:20 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://api.zhixiao.me:65508/zs/cn/cn.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
